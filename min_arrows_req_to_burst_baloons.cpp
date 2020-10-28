a2784e82fbfb8c60fddf1168d5da5a30f6d40f28
Mohak Gandhi <73507102+mohakgandhi862@users.noreply.github.com>
2020-10-28 08:02:18 +0530

    Add files via upload
    Minimum arrows required to burst balloons

---
+++ min_arrows_req_to_burst_baloons.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+int f(vector<int> A) {
+
+    int n = A.size();
+    map<int,int> m;
+    map<int, int> :: iterator itr;
+    int i, ans=0,tmp;
+
+    for (i = 0; i < n; i++) {
+        
+        itr = m.find(A[i] + 1);
+        if (itr == m.end()) {
+            ans++;
+        }
+        else {
+            tmp = itr->second;
+            m.erase(itr);
+            if(tmp>1) m.insert(make_pair(A[i]+1,tmp-1));
+        }
+        
+        itr = m.find(A[i]);
+        if (itr != m.end()) {
+            tmp = itr->second+1;
+            m.erase(itr);
+        }
+        else tmp = 1;
+        m.insert(make_pair(A[i], tmp));
+    }
+
+    return ans;
+}
+
+int main() {
+
+    vector<int> v = { 5, 4, 3, 3, 2, 2, 1, 1, 1 };
+    int ans = f(v);
+    cout << ans << endl;
+
+    return 0;
+}
